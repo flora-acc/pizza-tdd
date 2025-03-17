5a47114f42275d6e3aa159772af48d9903295306
flora.lucie.soler <flora.lucie.soler@accenture.com>
2025-03-17 10:16:33 +0100

    first commit

---
+++ src/main/java/com/accenture/pizza_tdd/PizzaTddApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.pizza_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/pizza_tdd/PizzaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.pizza_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
