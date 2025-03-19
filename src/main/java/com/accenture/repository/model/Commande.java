7690368bb1e023829cc40cc02c38db7422ba7937
flora.lucie.soler <flora.lucie.soler@accenture.com>
2025-03-19 09:46:20 +0100

    creation classe Commande

---
+++ src/main/java/com/accenture/repository/model/Commande.java
@@ -0,0 +1,16 @@
+package com.accenture.repository.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Commande {
+
+    int id;
+
+
+}
+
