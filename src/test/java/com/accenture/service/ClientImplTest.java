c68b22f18acff51cedd844e7c2ec1f3b6a5bc4b5
florian.bon <florian.bon@accenture.com>
2025-03-19 16:43:12 +0100

    fix

--- src/test/java/com/accenture/service/ClientImplTest.java
+++ src/test/java/com/accenture/service/ClientImplTest.java
@@ -36,76 +36,27 @@ class ClientImplTest {
     @Mock
     ClientDao clientDao;
 
-//    @Test
-//    void testAjoutClientNull() {
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(null));
-//        Assertions.assertEquals("Le client est introuvable", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientNomNull() {
-//        ClientRequestDto client1 = new ClientRequestDto("test", null,"test@email.fr");
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("Le nom est obligatoire", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientNomBlank() {
-//        ClientRequestDto client1 = new ClientRequestDto("test", "\t","test@email.fr");
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("Le nom est obligatoire", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientPreNomNull() {
-//        ClientRequestDto client1 = new ClientRequestDto(null, "test","test@email.fr");
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("Le prenom est obligatoire", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientPreNomBlank() {
-//        ClientRequestDto client1 = new ClientRequestDto("\t", "test","test@email.fr");
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("Le prenom est obligatoire", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientEmailNull() {
-//        ClientRequestDto client1 = new ClientRequestDto("test", "test",null);
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("L'email est obligatoire", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientEmailBlank() {
-//        ClientRequestDto client1 = new ClientRequestDto("test", "test","\t");
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("L'email est obligatoire", ex.getMessage());
-//    }
-//
-//    @Test
-//    void testAjoutClientEmailInvalide() {
-//        ClientRequestDto client1 = new ClientRequestDto("test", "test","formatInvalide");
-//        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
-//        Assertions.assertEquals("Format de l'email invalide", ex.getMessage());
-//    }
+    @Test
+    void testAjoutClientNull() {
+        ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(null));
+        Assertions.assertEquals("Le client est introuvable", ex.getMessage());
+    }
 
 
     @ParameterizedTest
-    @CsvSource(value={  "test,test,\t,L'email est obligatoire",
-                        "test,test,null,L'email est obligatoire",
-                        "test,test,formatInvalide,Format de l'email invalide",
-                        "null,test,test@email.fr,Le prenom est obligatoire",
-                        "\t,test,test@email.fr,Le prenom est obligatoire",
-                        "test,null,test@email.fr,Le nom est obligatoire",
-                        "test,\t,test@email.fr,Le nom est obligatoire"
-                        })
+    @CsvSource(value = {"test,test, ,L'email est obligatoire",
+            "test,test,null,L'email est obligatoire",
+            "test,test,formatInvalide,Format de l'email invalide",
+            "null,test,test@email.fr,Le prenom est obligatoire",
+            " ,test,test@email.fr,Le prenom est obligatoire",
+            "test,null,test@email.fr,Le nom est obligatoire",
+            "test, ,test@email.fr,Le nom est obligatoire"
+    })
     void testAjoutClientInvalide(String prenom, String nom, String email, String message) {
         prenom = transformNull(prenom);
         nom = transformNull(nom);
         email = transformNull(email);
-        ClientRequestDto client1 = new ClientRequestDto(prenom,nom,email);
+        ClientRequestDto client1 = new ClientRequestDto(prenom, nom, email);
         ClientException ex = Assertions.assertThrows(ClientException.class, () -> clientServiceImpl.ajouterClient(client1));
         Assertions.assertEquals(message, ex.getMessage());
     }
@@ -116,12 +67,12 @@ class ClientImplTest {
 
     @Test
     void testAjouterOk() {
-        ClientRequestDto clientRequestDto = new ClientRequestDto("Test", "Test","test@email.fr");
+        ClientRequestDto clientRequestDto = new ClientRequestDto("Test", "Test", "test@email.fr");
         Commande commande1 = new Commande(1);
-        ClientResponseDto clientResponseDto = new ClientResponseDto(1, "Test", "Test","test@email.fr", List.of(commande1));
+        ClientResponseDto clientResponseDto = new ClientResponseDto(1, "Test", "Test", "test@email.fr", List.of(commande1));
 
-        Client clientAvant = new Client("Test1","Test1","test@email.fr",List.of(commande1));
-        Client clientApres = new Client(1,"Test2","Test2","test@email.fr",List.of(commande1));
+        Client clientAvant = new Client("Test1", "Test1", "test@email.fr", List.of(commande1));
+        Client clientApres = new Client(1, "Test2", "Test2", "test@email.fr", List.of(commande1));
 
         Mockito.when(clientMapper.toClient(clientRequestDto)).thenReturn(clientAvant);
         Mockito.when(clientDao.save(clientAvant)).thenReturn(clientApres);
